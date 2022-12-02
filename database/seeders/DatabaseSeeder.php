e2093431b365b12f18c1f32004811e9dbae91980
anigato <203040096@mail.unpas.ac.id>
2022-12-02 16:28:24 +0700

    menambahkan seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name'=>'adminnya saya',
+            'username'=>'admin123',
+            'email'=>'admin123@gmail.com',
+            'password'=>bcrypt('admin123'),
+            'is_admin'=>true
+        ]);
+
+        User::create([
+            'name'=>'usernya saya',
+            'username'=>'user123',
+            'email'=>'user123@gmail.com',
+            'password'=>bcrypt('user123'),
+            'is_admin'=>false
+        ]);
     }
 }
