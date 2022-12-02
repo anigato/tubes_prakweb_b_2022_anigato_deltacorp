eb593f04f70c044d4fa1e7429fdd60a829bf0f40
QistiIzatus <76423044+QistiIzatus@users.noreply.github.com>
2022-12-02 18:27:24 +0700

    membuat halaman login user

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -21,7 +21,7 @@
     @endif
 
         <main class="form-signin w-100 m-auto">  
-        <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>  
+        <h1 class="h3 mb-3 fw-normal text-center">Please Login User</h1>  
         <form action="/login" method="post">
             @csrf
             <div class="form-floating">
