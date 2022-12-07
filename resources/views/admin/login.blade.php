448386a2c61526d49a10fbb808ef7ef38bb0790f
anigato <203040096@mail.unpas.ac.id>
2022-12-07 23:02:36 +0700

    memperbaiki tampilan login admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,50 +1,66 @@
-@extends('admin.layouts.main')
+<!doctype html>
+<html lang="en">
 
-@section('container')
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div class="row justify-content-center">
-  <div class="col-md-4">
+    {{-- bootstrap css --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    @if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      {{ session('success') }}
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
+    {{-- fontawesome --}}
+    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet">
+    </link>
+
+    {{-- jquery --}}
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    {{-- sweetalert --}}
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    {{-- custom css login --}}
+    <link rel="stylesheet" href="../theme/backend/css/login.css">
+    <title>DeltaCorp | {{ $title }}</title>
+</head>
+
+<body>
+
+    @if (session()->has('loginError'))
+        <script type='text/javascript'>
+            Swal.fire({
+                title: 'Maaf!',
+                text: '{{ session('loginError') }}',
+                icon: 'error',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'OK'
+            })
+        </script>
     @endif
 
-    @if(session()->has('loginError'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      {{ session('loginError') }}
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="login-reg-panel">
+        <div class="white-panel">
+            <div class="login-show show-log-panel mt-5">
+                <h2>MASUK</h2>
+                <form action="/login" method="post">
+                    @csrf
+                    <input type="hidden" name="is_admin" readonly value="1">
+
+                    <input type="email" name="email" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+
+                    <input type="password" name="password" id="password" placeholder="Password" required>
+
+                    <button type="submit" name="login" class="submit">Masuk</button>
+
+                </form>
+            </div>
+        </div>
     </div>
-    @endif
 
-    <main class="form-signin">
-      <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-      <form action="/login" method="post">
-        @csrf
-        <input type="hidden" name="is_admin" readonly value="1">
-        <div class="form-floating">
-          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-          <label for="email">Email address</label>
-          @error('email')
-          <div class="invalid-feedback">
-            {{ $message }}
-          </div>
-          @enderror
-        </div>
-        <div class="form-floating">
-          <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-          <label for="password">Password</label>
-        </div>
-
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-      </form>
-
-      <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
-
-    </main>
-  </div>
-</div>
-
-@endsection
+    {{-- boostrap js --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
