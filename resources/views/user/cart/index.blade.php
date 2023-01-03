4c5fb9c47f4ddc0a583cd034abbb5e9e23007563
anigato <203040096@mail.unpas.ac.id>
2023-01-03 22:11:20 +0700

    memperbaiki semua halaman yang diakses user

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -1,41 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
-    {{-- sweetalert --}}
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    @include('user.layouts.parts.link-head')
-    <title>DeltaCorp - Keranjang</title>
-</head>
-
-<body>
-    @if (session()->has('success'))
-    <script type='text/javascript'>
-        Swal.fire({
-            title: 'Success!',
-            text: '{{ session('success') }}',
-            icon: 'success',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'OK'
-        })
-    </script>
-    @endif
-
-    <!-- End header area -->
-    @include('user.layouts.parts.header')
-
-
-    <!-- End site branding area -->
-    @include('user.layouts.parts.branding-area')
-
-
-    <!-- End mainmenu area -->
-    @include('user.layouts.parts.main-menu')
+@extends('user.layouts.main')
+@section('container')
+    <?php
+    function rupiah($harga)
+    {
+        $hasil_harga = 'Rp. ' . number_format($harga, 0, ',', '.');
+        return $hasil_harga;
+    }
+    ?>
     <div class="product-big-title-area">
         <div class="container">
             <div class="row">
@@ -57,8 +28,8 @@
                     <table class="table table-condensed">
 
                         <?php
-                        if (isset($_SESSION['cart'])) {
-                        ?>
+                if (isset($_SESSION['cart'])) {
+                ?>
                         <tr>
                             <td>Foto</td>
                             <td>Nama Produk</td>
@@ -69,15 +40,15 @@
                         </tr>
 
                         <?php
-                            $cart = unserialize(serialize($_SESSION['cart']));
-                            $index = 0;
-                            $total = 0;
-                            $total_bayar = 0;
+                    $cart = unserialize(serialize($_SESSION['cart']));
+                    $index = 0;
+                    $total = 0;
+                    $total_bayar = 0;
 
-                            for ($i = 0; $i < count($cart); $i++) {
-                                $total = $_SESSION['cart'][$i]['price'] * $_SESSION['cart'][$i]['qty'];
-                                $total_bayar += $total;
-                            ?>
+                    for ($i = 0; $i < count($cart); $i++) {
+                        $total = $_SESSION['cart'][$i]['price'] * $_SESSION['cart'][$i]['qty'];
+                        $total_bayar += $total;
+                    ?>
 
                         <tr>
                             <td>
@@ -94,16 +65,18 @@
                                     <input type="hidden" name="product_id" id="product_id"
                                         value="{{ $cart[$i]['product_id'] }}">
                                     <div class="row">
-                                        <input type="number" class="form-control col-md-5" name="qty_update" value="{{ $cart[$i]['qty'] }}" onkeyup="submit()">
-                                        <button type="submit" class="form-control col-md-3 btn btn-sm"><i class="fas fa-check"></i></button>
+                                        <input type="number" class="form-control col-md-5" name="qty_update"
+                                            value="{{ $cart[$i]['qty'] }}" onkeyup="submit()">
+                                        <button type="submit" class="form-control col-md-3 btn btn-sm"><i
+                                                class="fas fa-check"></i></button>
                                     </div>
                                 </form>
                             </td>
 
-                            <td>{{ $cart[$i]['price'] }}</td>
-                            <td>{{ $total }}</td>
+                            <td>{{ rupiah($cart[$i]['price']) }}</td>
+                            <td>{{ rupiah($total) }}</td>
                             <td>
-                                <form action="{{ url('cart/'.$index ) }}" method="post">
+                                <form action="{{ url('cart/' . $index) }}" method="post">
                                     @csrf
                                     @method('delete')
                                     <input type="hidden" name="index" id="index" value="{{ $index }}">
@@ -113,11 +86,11 @@
                         </tr>
 
                         <?php
-                            $index++;
-                        }?>
+                    $index++;
+                }?>
                         <tr>
                             <td colspan="4"><strong>Total Bayar</strong></td>
-                            <td colspan="2"><strong>{{ $total_bayar }}</strong></td>
+                            <td colspan="2"><strong>{{ rupiah($total_bayar) }}</strong></td>
                             <td>
                                 <form action="{{ url('cart/checkout') }}" method="post">
                                     @csrf
@@ -126,15 +99,11 @@
                             </td>
                         </tr>
                         <?php
-                        } else {
-                        echo "<h1 class='text-danger'>Anda Tidak memiliki produk di dalam keranjang</h1>";
-                        
-                        }
-                        ?>
-
-
-
-
+                } else {
+                echo "<h1 class='text-danger'>Anda Tidak memiliki produk di dalam keranjang</h1>";
+                
+                }
+                ?>
                     </table>
                 </div>
 
@@ -153,90 +122,4 @@
             </div>
         </div>
     </div>
-
-
-    <!-- End brands area -->
-    @include('user.layouts.parts.brands-area')
-
-
-    <!-- End footer bottom area -->
-    @include('user.layouts.parts.footer')
-
-    <!--End script-body-->
-    @include('user.layouts.parts.script-body')
-    <?php
-    if (isset($_POST['checkout'])) {
-        $user = $_SESSION['user_name'];
-        $user = query("SELECT*FROM users WHERE username = '$user'")[0];
-        $uid = $user['id'];
-        $addres = query("SELECT*FROM address WHERE id_user = $uid")[0];
-        if (!empty($addres['provinsi'])) {
-            $status = checkout();
-            if ($status == 'outstock') {
-                echo "
-                                <script type='text/javascript'>
-                                Swal.fire({
-                                    title:'Opps!',
-                                    text:'Maaf pesanan anda melebihi stok, silahkan cek kembali',
-                                    icon:'error',
-                                    confirmButtonColor: '#3085d6',
-                                    confirmButtonText: 'OK'
-                                }).then((result) => {
-                                    if (result.value) {
-                                    document.location.href='../cart/index.php';
-                                    }
-                                })
-                                </script>
-                            ";
-            } elseif ($status == 'success') {
-                echo "
-                                <script type='text/javascript'>
-                                
-                                Swal.fire({
-                                    title:'Berhasil!',
-                                    text:'Anda berhasil memesan, silahkan lanjutkan pembayaran',
-                                    icon:'success',
-                                    confirmButtonColor: '#3085d6',
-                                    confirmButtonText: 'OK'
-                                }).then((result) => {
-                                    if (result.value) {
-                                    document.location.href='../transaction/index.php';
-                                    }
-                                })
-                                </script>
-                            ";
-            }
-        } else {
-            echo "
-                                <script type='text/javascript'>
-                                Swal.fire({
-                                    title:'Maaf!',
-                                    text:'Sebelum melakukan pemesanan, anda harus mengisi alamat dengan lengkap',
-                                    icon:'warning',
-                                    confirmButtonColor: '#3085d6',
-                                    confirmButtonText: 'OK'
-                                }).then((result) => {
-                                    if (result.value) {
-                                    document.location.href='../user/detail.php';
-                                    }
-                                })
-                                </script>
-                            ";
-        }
-    }
-    
-    ?>
-    <script>
-        // input form khusus nomor
-        function onlyNumber(evt) {
-            var charCode = (evt.which) ? evt.which : event.keyCode
-            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    </script>
-</body>
-
-</html>
+@endsection
--- resources/views/user/layouts/parts/related-product.blade.php
+++ resources/views/user/layouts/parts/related-product.blade.php
@@ -2,21 +2,21 @@
 <div class="related-products-wrapper">
     <h2 class="related-products-title">Mungkin Kamu Suka</h2>
     <div class="related-products-carousel">
-                @foreach ($randomProducts as $randomProduct)
-                <div class="single-product">
-                    <div class="product-f-image">
-                        <img src="{{ asset('storage/img/product/'.$randomProduct['img']) }}" alt="">
-                        <div class="product-hover">
-                            <a href="{{ url('product/'.$randomProduct['id']) }}" class="view-details-link"><i class="fa fa-link"></i> Lihat Detail</a>
-                        </div>
-                    </div>
-
-                    <h2><a class="text-uppercase" href="{{ url('product/'.$randomProduct['id']) }}">{{ $randomProduct['name'] }}</a></h2>
-
-                    <div class="product-carousel-price">
-                        <h2><ins>{{ $randomProduct['price'] }}</ins></h2>
-                    </div>
+        @foreach ($randomProducts as $randomProduct)
+        <div class="single-product">
+            <div class="product-f-image">
+                <img src="{{ asset('storage/img/product/'.$randomProduct['img']) }}" alt="">
+                <div class="product-hover">
+                    <a href="{{ url('product/'.$randomProduct['id']) }}" class="view-details-link"><i class="fa fa-link"></i> Lihat Detail</a>
                 </div>
-                @endforeach
+            </div>
+
+            <h2><a class="text-uppercase" href="{{ url('product/'.$randomProduct['id']) }}">{{ $randomProduct['name'] }}</a></h2>
+
+            <div class="product-carousel-price">
+                <h2><ins>{{ rupiah($randomProduct['price']) }}</ins></h2>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
